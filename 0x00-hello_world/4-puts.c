45dfae2ce81fca611ce5da8de2ae371579903732
mohomer24 <blue.dream788@gmail.com>
2023-10-09 06:39:04 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-       puts("Programming is like building a multilingual puzzle\n");
+       puts("/"Programming is like building a multilingual puzzle");
 		return (0);
-		;
+
 }
 	
